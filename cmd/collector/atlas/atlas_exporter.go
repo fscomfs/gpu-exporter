945e4007bf92e7f0b0f722e9be5048b2d772bf61
周小锋 <xiaofeng.zhou@extremevision.com.cn>
2023-03-21 17:43:44 +0800

    atlas 适配 dcmi

--- cmd/collector/atlas/atlas_exporter.go
+++ cmd/collector/atlas/atlas_exporter.go
@@ -3,7 +3,8 @@ package atlas
 import (
 	"fmt"
 	"github.com/fscomfs/gpu-exporter/cmd/collector"
-	"github.com/fscomfs/gpu-exporter/cmd/collector/atlas/dsmi"
+	"github.com/fscomfs/gpu-exporter/cmd/collector/atlas/common"
+	"github.com/fscomfs/gpu-exporter/cmd/collector/atlas/dcmi"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/cast"
 	"log"
@@ -13,6 +14,7 @@ import (
 )
 
 var disabledFlag = false
+var dc *dcmi.DcManager
 
 func init() {
 	defer func() {
@@ -21,9 +23,15 @@ func init() {
 			disabledFlag = true
 		}
 	}()
-	dsmi.Init()
-	collector.Register("npu", new())
-	log.Printf("dsmi init success")
+	dc = &dcmi.DcManager{}
+	defer dc.DcShutDown()
+	err := dc.DcInit()
+	if err == nil {
+		collector.Register("npu", new())
+		log.Printf("dsmi init success")
+	} else {
+		disabledFlag = true
+	}
 
 }
 
@@ -61,19 +69,28 @@ func (e *AtlasExporter) Collect(metricCh chan<- prometheus.Metric) {
 	if disabledFlag {
 		return
 	}
-	deviceCount, r := dsmi.GetDeviceCount()
+	dc.DcInit()
+	defer dc.DcShutDown()
+	deviceCount, r := dc.DcGetDeviceCount()
 	if r == nil && deviceCount > 0 {
-		allInfo, err := dsmi.AllDeviceInfo()
-		if err == nil {
-			for deviceIndex, deviceInfo := range allInfo {
-				usedMemory := uint64(deviceInfo.CoreRate) * deviceInfo.Total / 100
-				used := prometheus.MustNewConstMetric(e.gpuInfoDesc, prometheus.GaugeValue, float64(usedMemory), cast.ToString(deviceIndex), collector.MemoryUsed, deviceInfo.ChipName, collector.NPU, runtime.GOARCH)
+		_, carList, _ := dc.DcGetCardList()
+		index := 0
+		for _, carIndex := range carList {
+			deviceIdMax, _ := dc.DcGetDeviceNumInCard(carIndex)
+			for deviceId := int32(0); deviceId < deviceIdMax; deviceId++ {
+				memoryInfo, _ := dc.DcGetMemoryInfo(carIndex, deviceId)
+				chipInfo, _ := dc.DcGetChipInfo(carIndex, deviceId)
+				coreRate, _ := dc.DcGetDeviceUtilizationRate(carIndex, deviceId, common.AICore)
+				usedMemory := uint64(coreRate) * memoryInfo.MemorySize / 100
+				used := prometheus.MustNewConstMetric(e.gpuInfoDesc, prometheus.GaugeValue, float64(usedMemory), cast.ToString(index), collector.MemoryUsed, chipInfo.Name, collector.NPU, runtime.GOARCH)
 				metricCh <- used
-				free := prometheus.MustNewConstMetric(e.gpuInfoDesc, prometheus.GaugeValue, float64(deviceInfo.Total-usedMemory), cast.ToString(deviceIndex), collector.MemoryFree, deviceInfo.ChipName, collector.NPU, runtime.GOARCH)
+				free := prometheus.MustNewConstMetric(e.gpuInfoDesc, prometheus.GaugeValue, float64(memoryInfo.MemoryAvailable), cast.ToString(index), collector.MemoryFree, chipInfo.Name, collector.NPU, runtime.GOARCH)
 				metricCh <- free
-				total := prometheus.MustNewConstMetric(e.gpuInfoDesc, prometheus.GaugeValue, float64(deviceInfo.Total), cast.ToString(deviceIndex), collector.MemoryTotal, deviceInfo.ChipName, collector.NPU, runtime.GOARCH)
+				total := prometheus.MustNewConstMetric(e.gpuInfoDesc, prometheus.GaugeValue, float64(memoryInfo.MemorySize), cast.ToString(index), collector.MemoryTotal, chipInfo.Name, collector.NPU, runtime.GOARCH)
 				metricCh <- total
+				index++
 			}
 		}
+
 	}
 }
