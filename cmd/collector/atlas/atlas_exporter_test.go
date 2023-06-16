414e3f1368a60b762329499b85094f70cd2288ee
zhouxiaofeng <1041786715@qq.com>
2023-06-16 18:00:35 +0800

    atlas test

--- cmd/collector/atlas/atlas_exporter_test.go
+++ cmd/collector/atlas/atlas_exporter_test.go
@@ -1,6 +1,8 @@
 package atlas
 
 import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"testing"
 )
 
@@ -13,4 +15,23 @@ func TestSum(t *testing.T) {
 	//w = 999
 	//b := w * 1024 * 1024
 	//fmt.Printf("total:%+v", b)
+
+	fmt.Println("test1")
+	export := new(true)
+	metrics := make(chan prometheus.Metric)
+	export.Collect(metrics)
+
+}
+
+func TestSum2(t *testing.T) {
+	//a := dsmi.AtlasInfo{
+	//	Total: uint32(9000),
+	//	Used:  222,
+	//}
+	//var w C.int32
+	//w = 999
+	//b := w * 1024 * 1024
+	//fmt.Printf("total:%+v", b)
+	fmt.Println("test2")
+
 }
